b5e8aaece7f4df5ab4bbb3d39664b3ed1c8bb0c9
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-11-13 14:18:46 -0800

    Add project files.

---
+++ Stat Card Sell Price/Mod.cs
@@ -0,0 +1,18 @@
+﻿using PulsarModLoader;
+
+namespace StatCardSellPrice
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "StatCardSellPrice";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
---
+++ Stat Card Sell Price/Patches.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+
+namespace StatCardSellPrice
+{
+    //Universal Components
+    [HarmonyPatch(typeof(PLShipComponent), "GetExtraLineLeft")]
+    internal class StatLineLeftComponentPatch
+    {
+        static void Postfix(ref string __result)
+        {
+            __result = __result + $"{PLLocalize.Localize("Price")}\n";
+        }
+    }
+    [HarmonyPatch(typeof(PLShipComponent), "GetExtraLineRight")]
+    internal class StatLineRightComponentPatch
+    {
+        static void Postfix(PLWare __instance, ref string __result)
+        {
+            __result = __result + $"{__instance.GetScaledMarketPrice(true)}\n";
+        }
+    }
+
+    //Universal Items
+    [HarmonyPatch(typeof(PLWare), "GetExtraLineLeft")]
+    internal class StatLineLeftPawnItemPatch
+    {
+        static void Postfix(ref string __result)
+        {
+            __result = __result + $"{PLLocalize.Localize("Price")}\n";
+        }
+    }
+    [HarmonyPatch(typeof(PLWare), "GetExtraLineRight")]
+    internal class StatLineRightPawnItemPatch
+    {
+        static void Postfix(PLWare __instance, ref string __result)
+        {
+            __result = __result + $"{__instance.GetScaledMarketPrice(true)}\n";
+        }
+    }
+
+    //Extractor Components
+    [HarmonyPatch(typeof(PLExtractor), "GetExtraLineLeft")]
+    internal class StatLineLeftExtractorPatch
+    {
+        static void Postfix(ref string __result)
+        {
+            __result = __result + $"{PLLocalize.Localize("Price")}\n";
+        }
+    }
+    [HarmonyPatch(typeof(PLExtractor), "GetExtraLineRight")]
+    internal class StatLineRightExtractorPatch
+    {
+        static void Postfix(PLWare __instance, ref string __result)
+        {
+            __result = __result + $"{__instance.GetScaledMarketPrice(true)}\n";
+        }
+    }
+
+    //Program Components
+    [HarmonyPatch(typeof(PLWarpDriveProgram), "GetExtraLineLeft")]
+    internal class StatLineLeftProgramsPatch
+    {
+        static void Postfix(ref string __result)
+        {
+            __result = __result + $"{PLLocalize.Localize("Price")}\n";
+        }
+    }
+    [HarmonyPatch(typeof(PLWarpDriveProgram), "GetExtraLineRight")]
+    internal class StatLineRightProgramsPatch
+    {
+        static void Postfix(PLWare __instance, ref string __result)
+        {
+            __result = __result + $"{__instance.GetScaledMarketPrice(true)}\n";
+        }
+    }
+}
